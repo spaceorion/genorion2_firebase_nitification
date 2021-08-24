71a36ba2f6601c3ad7cb0102c083353e95dead77
pankajpalariya <pankajpalariya21@gmail.com>
2021-08-24 11:14:03 +0530

    asdlkfj

--- myproject/settings.py
+++ myproject/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'crispy_forms',
     'django_cleanup',
-    'background_task',
+    # 'background_task',
     # 'organizations',
     # 'phone_login',
 ]
