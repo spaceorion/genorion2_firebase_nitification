067f3dfc3fff687e2c8812d7e419b5c79ec39425
pankajpalariya <pankajpalariya21@gmail.com>
2021-10-15 13:19:57 +0530

    dfasd

--- myapp/migrations/0001_initial.py
+++ myapp/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0012_alter_user_first_name_max_length'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
