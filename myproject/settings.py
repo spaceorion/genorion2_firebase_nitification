c7e1ba59263d26c9ad285bd60c44df0a8ec51c7d
Pratap Kumar <pk949542@gmail.com>
2022-10-11 12:36:47 +0530

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -255,4 +255,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'spaceorion2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'space@2020@'
+EMAIL_HOST_PASSWORD = 'flgtdjikabtzvpvv'
